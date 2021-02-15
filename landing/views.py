363fff68bf0e71ed2b7742746acb97878e280168
badsmile <hsabib@protonmail.com>
2021-02-15 04:04:33 +0100

    send msg

--- landing/views.py
+++ landing/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.core.mail import send_mail
 
 class IndexView(View):
     def get(self, request) :
@@ -22,3 +22,17 @@ class ContactView(View):
         if light_version == "1" :
             template = 'landing/contact_light.html'
         return render(request, template)
+    def post(self, request):
+        name = request.POST.get('name', None)
+        email = request.POST.get('email', None)
+        number = request.POST.get('number', None)
+        message = request.POST.get('message', None)
+
+        send_mail(
+            'Shifty project',
+            message,
+            'killersabib@gmail.com',
+            ['hsabib@protonmail.com'],
+            fail_silently=False,
+        )
+        return redirect('/contact/?msg_sent=true')
