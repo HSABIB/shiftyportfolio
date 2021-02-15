2dbb025ee3772880824383302a3a0843a0b43c77
badsmile <hsabib@protonmail.com>
2021-02-15 15:43:17 +0100

    add contact att6 -- change message format

--- landing/views.py
+++ landing/views.py
@@ -30,7 +30,7 @@ class ContactView(View):
 
         send_mail(
             'Shifty project',
-            '<b>Phone :</b> 'number + '<br /><b>Message :</b>' + message,
+            '<b>Phone :</b> ' + number + '<br /><b>Message :</b>' + message,
             'killersabib@gmail.com',
             ['hsabib@protonmail.com'],
             fail_silently=False,
