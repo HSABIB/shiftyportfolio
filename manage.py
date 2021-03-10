b4b14ee03265808480fc1bd6d1f3b78cd98a4da9
badsmile <hsabib@protonmail.com>
2021-03-10 13:35:25 +0100

    change to production

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.developement')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
